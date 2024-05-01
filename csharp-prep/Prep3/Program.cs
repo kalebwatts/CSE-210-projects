178c5c1f076f0905b58ab38da59de5022fd1513e
kalebwatts <kalebwatts40@gmail.com>
2024-04-30 21:08:58 -0600

    the code stuff for prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // For parts 1 and 2
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101);
+
+        Console.WriteLine("Welcome to Guess My Number Game!");
+        Console.WriteLine("I've picked a magic number between 1 and 100.");
+        Console.WriteLine("Try to guess it!");
+
+        int guess;
+        do
+        {
+            Console.Write("Enter your guess: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
     }
 }
